414616da0ebc6c8ec23983db4fff24648bb0532d
Cassandra Comar <cass@ndra.io>
2025-03-23 11:58:31 -0500

    Make llvm-hs and llvm-hs-pure Compile Again
    The project should now be able to compile with new headers that llvm 16
    shipped with.

--- llvm-hs/Setup.hs
+++ llvm-hs/Setup.hs
@@ -9,6 +9,11 @@ import Distribution.PackageDescription hiding (buildInfo, includeDirs)
 import Distribution.Simple
 import Distribution.Simple.LocalBuildInfo
 import Distribution.Simple.PreProcess
+#ifdef MIN_VERSION_Cabal
+#if MIN_VERSION_Cabal(3,12,0)
+import Distribution.Simple.PreProcess.Types (Suffix)
+#endif
+#endif
 import Distribution.Simple.Program
 import Distribution.Simple.Setup hiding (Flag)
 import Distribution.System
@@ -20,6 +25,9 @@ import System.Environment
 #endif
 #if MIN_VERSION_Cabal(3,8,1)
 #define MIN_VERSION_Cabal_3_8_1
+#endif
+#if MIN_VERSION_Cabal(3,12,0)
+#define MIN_VERSION_Cabal_3_12_0
 #endif
 #endif
 
@@ -185,14 +193,21 @@ main = do
               where origHsc buildInfo' =
                       fromMaybe
                         ppHsc2hs
+#ifdef MIN_VERSION_Cabal_3_12_0
+                        (lookup (Suffix "hsc") origHookedPreprocessors)
+#else
                         (lookup "hsc" origHookedPreprocessors)
+#endif
                         buildInfo'
                         localBuildInfo
 #ifdef MIN_VERSION_Cabal_2_0_0
                         componentLocalBuildInfo
 #endif
+#ifdef MIN_VERSION_Cabal_3_12_0
+      in [(Suffix "hsc", newHsc)] ++ origHookedPreprocessors,
+#else
       in [("hsc", newHsc)] ++ origHookedPreprocessors,
-
+#endif
     buildHook = \packageDesc localBuildInfo userHooks buildFlags ->
       do addLLVMToLdLibraryPath (configFlags localBuildInfo)
          buildHook origUserHooks packageDesc localBuildInfo userHooks buildFlags,
